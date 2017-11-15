71a533955874decd03815bf420d2b529c8ef2aec
kultie <putin1721996@gmail.com>
2017-11-15 14:01:08 +0700

    15/11/2017
    Add Dead Fly

--- Rambo/Assets/Script/FlyController.cs
+++ Rambo/Assets/Script/FlyController.cs
@@ -2,37 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeController : MonoBehaviour {
-    public float speed;
+public class FlyController : MonoBehaviour {
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask WhatIsGround;
     public bool grounded;
-
+    public float speed;
+    public float slowSpeed;
     public GameObject groundHitBox;
     private Animator anim;
     private Rigidbody2D myrigidbody2D;
     EnemyPatrol ePatrol;
     EnemyManager eManager;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = GetComponent<Animator>();
         ePatrol = GetComponent<EnemyPatrol>();
         eManager = GetComponent<EnemyManager>();
-        ePatrol.moveSpeed = speed;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        myrigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void detectPlayer() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right * -1*(System.Convert.ToInt32(ePatrol.facingLeft)),5f);
+        Debug.DrawRay(transform.position, Vector3.right * -5 * (System.Convert.ToInt32(ePatrol.facingLeft)), Color.red, 2f, false);
+        if (hit.collider.CompareTag("Player"))
+        {
+            StartCoroutine(shoot());
+        }
+    }
+    private void FixedUpdate()
+    {
+        detectPlayer();
+    }
+    // Update is called once per frame
+    void Update()
+    {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
-        anim.SetBool("Ground", grounded);
         if (!grounded)
         {
-            ePatrol.canMove = false;
+            ePatrol.moveSpeed = slowSpeed;
             groundHitBox.GetComponent<Collider2D>().enabled = false;
         }
-        else {
-            ePatrol.canMove = true;
+        else
+        {
+            ePatrol.moveSpeed = speed;
             groundHitBox.GetComponent<Collider2D>().enabled = true;
         }
         if (eManager.enemyHP <= 0)
@@ -41,9 +54,17 @@ public class SlimeController : MonoBehaviour {
             StartCoroutine(death());
         }
     }
-    IEnumerator death() {
+    IEnumerator death()
+    {
         anim.SetBool("Dead", true);
         yield return new WaitForSeconds(0.76f);
         Destroy(this.gameObject);
+    }
+    IEnumerator shoot()
+    {
+        anim.SetBool("Shooting", true);
+        yield return new WaitForSeconds(0.76f);
+        Debug.Log("Shooting");
+        anim.SetBool("Shooting", false);
     }
 }
